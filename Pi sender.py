ec93ffd4c730b1973c6b41f31bfe5373a43632da
Oleksandr Hurin <gurinaleksandr27032008@gmail.com>
2026-06-29 13:23:23 +0200

    Raspberry pie zwischenergebniss 29.06

--- Pi sender.py
+++ Pi sender.py
@@ -2,7 +2,7 @@ import socket
 import struct
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 ip = "192.168.1.102"
-ip_receiver = "192.168.1.103"
+ip_receiver = "192.168.1.86"
 port=int("9999")
 nachricht = input("Deine Nachricht?")
 data = nachricht.encode("utf-8")
--- Pi server.py
+++ Pi server.py
@@ -1,11 +1,14 @@
 import socket
 import struct
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('192.168.1.102', 9999))
+sock.bind(('0.0.0.0', 9999))
+sock_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock_send.bind(("192.168.1.48", 0))
 port=int("9999")
 while True:
     data, adress=sock.recvfrom(1024)
     checksum, length, ip_encoded = struct.unpack("!HH4s", data[0:8])
     ip_receiver = socket.inet_ntoa(ip_encoded)
     print("sending to" + ip_receiver)
-    sock.sendto(data,(ip_receiver,port))
+    sock_send.sendto(data,(ip_receiver,port))
+    print("sent to " + ip_receiver + " via wlan0")
